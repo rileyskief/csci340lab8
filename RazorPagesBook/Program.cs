b651e39f0564ce0a26913e5fe33bec9509f6025d
Riley Skief <skiefri@hendrix.edu>
2023-11-20 16:13:03 -0600

    scaffold

--- RazorPagesBook/Program.cs
+++ RazorPagesBook/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesBook.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesBookContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesBookContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesBookContext' not found.")));
 
 var app = builder.Build();
 
