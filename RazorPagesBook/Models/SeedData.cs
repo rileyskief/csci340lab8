11f05fd7a0bbf3653b2272a279856079c5afa392
Riley Skief <skiefri@hendrix.edu>
2023-11-24 16:55:18 -0600

    finished product

--- RazorPagesBook/Models/SeedData.cs
+++ RazorPagesBook/Models/SeedData.cs
@@ -26,32 +26,32 @@ public static class SeedData
                 new Book
                 {
                     Title = "The Hunt for Red October",
-                    PublishedDate = DateTime.Parse("1984-10-1"),
                     Author = "Tom Clancy",
+                    PublishedDate = DateTime.Parse("1984-10-1"),
                     Price = 9.89M
                 },
 
                 new Book
                 {
                     Title = "All That Remains",
-                    PublishedDate = DateTime.Parse("1992-4-15"),
                     Author = "Patricia Cornwell",
+                    PublishedDate = DateTime.Parse("1992-4-15"),
                     Price = 8.50M
                 },
 
                 new Book
                 {
                     Title = "Gone With the Wind",
-                    PublishedDate = DateTime.Parse("1936-6-30"),
                     Author = "Margaret Mitchell",
+                    PublishedDate = DateTime.Parse("1936-6-30"),
                     Price = 18.58M
                 },
 
                 new Book
                 {
                     Title = "White Jacket",
-                    PublishedDate = DateTime.Parse("1850-1-23"),
                     Author = "Herman Melville",
+                    PublishedDate = DateTime.Parse("1850-1-23"),
                     Price = 19.95M
                 }
             );
